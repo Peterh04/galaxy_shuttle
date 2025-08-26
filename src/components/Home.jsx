806b179986f6f5a2c61cfa84b5f574db67f3bfb3
Peter Heri <peterheri@Peters-MacBook-Pro.local>
2025-08-26 12:12:22 +0300

    feat:Add Home Page with level btns and music button
    Users can now mute and unmute the music and they can also pick which  difficulty they want to play from the home screen

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,37 @@
 import Levels from "./Levels";
 import "../styles/home.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVolumeHigh, faVolumeMute } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+import spaceMusic from "../assets/space-music.mp3";
+import { Howl } from "howler";
+import ReactHowler from "react-howler";
+
 export default function Home() {
+  const [isMute, setIsMute] = useState(true);
+
+  const handleMute = () => {
+    setIsMute((prev) => !prev);
+  };
   return (
     <div className="home">
       <h2>GALAXY SHUFFLE</h2>
       <Levels />
-      <button className="muteBtn">Mute</button>
+
+      <ReactHowler
+        src={spaceMusic}
+        playing={!isMute}
+        loop={true}
+        volume={0.1}
+      />
+      <button className="muteBtn" onClick={() => handleMute()}>
+        {isMute ? (
+          <FontAwesomeIcon icon={faVolumeMute} className="fa" />
+        ) : (
+          <FontAwesomeIcon icon={faVolumeHigh} className="fa" />
+        )}
+      </button>
     </div>
   );
 }
